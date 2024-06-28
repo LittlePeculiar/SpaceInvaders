4e10ea77610628f944c14aafaa9cc5ad69dbd64f
Gina Mullins <LittlePeculiar13@gmail.com>
2024-06-28 08:11:33 -0700

    Initial Commit

---
+++ SpaceInvaders/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SpaceInvaders
+//
+//  Created by Gina Mullins on 6/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
